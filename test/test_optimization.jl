c34dc35998b5e8c33a2a51ee109717c52bde7500
arnab82 <arnab22@vt.edu>
2025-04-05 11:18:44 -0400

    nsites 6, nlevels 2, nsites 3 nlevels 3  optimization is working

--- test/test_optimization.jl
+++ test/test_optimization.jl
@@ -11,16 +11,19 @@ using LineSearches
     n_samples = 300
     δt = T/n_samples
     Random.seed!(2)
-    freqs = [0.2,0.22]
-    freqs = [0.1,0.24,0.6]
+    freqs = [0.2,0.22]#2,2
+    freqs = [0.1,0.24,0.6]#2,3
+    # freqs = [0.6,0.15,0.3]#3,3
     # freqs = [0.4,0.8,0.1,0.1 ]
+    freqs = [0.8,0.4,0.08,0.23]
     # freqs = [0.5,0.6,0.3,0.03,0.65]
-    freqs = [0.1,0.55,0.5,0.06,0.25,0.3]
+    # freqs = [0.1,0.55,0.5,0.06,0.25,0.3]
+    # freqs = [0.08,0.3,0.3,0.45,0.3,0.27,0.28]
     signals_ = [DigitizedSignal([sin(2π*(t/n_samples)) for t in 0:n_samples+1], δt, f) for f in freqs]
     signals = MultiChannelSignal(signals_)
 
-    n_sites = 6
-    n_levels = 2
+    n_sites = 4
+    n_levels = 3
 
     dim = n_levels^n_sites
     Hstatic = rand(Float64, dim, dim) 
@@ -58,9 +61,9 @@ using LineSearches
     Λ, U = eigen(C)
     E_actual = Λ[1]
     println("Actual energy: $E_actual") 
+
     # we have to optimize the samples in the signal
-    
-    n_samples_grad = 200
+    n_samples_grad = 150
     δΩ_ = Matrix{Float64}(undef, n_samples+1, n_sites)
     ∂Ω0 = Matrix{Float64}(undef, n_samples_grad+1, n_sites)
     τ = T/n_samples_grad
@@ -124,14 +127,19 @@ using LineSearches
     samples_final = Optim.minimizer(optimization)      # FINAL PARAMETERS
     optimization = Optim.optimize(costfunction, gradient_ode!, samples_final, optimizer, options)
     samples_final = Optim.minimizer(optimization) 
-    # optimization = Optim.optimize(costfunction, gradient_ode!, samples_final, optimizer, options)
-    # samples_final = Optim.minimizer(optimization)
-    # optimization = Optim.optimize(costfunction, gradient_ode!, samples_final, optimizer, options)
-    # samples_final = Optim.minimizer(optimization)
-    # optimization = Optim.optimize(costfunction, gradient_ode!, samples_final, optimizer, options)
-    # samples_final = Optim.minimizer(optimization)
+    samples_final_reshaped = reshape(samples_final, n_samples+1, n_sites)
+    
     Λ, U = eigen(C)
     E_actual = Λ[1]
     println("Actual energy: $E_actual")
+
+    Ω0=copy(samples_matrix)
+    Ω = copy(samples_final_reshaped)
+    plot(Ω0[:,1], label="initial signal ", color=:black, lw=2, linestyle=:dash)
+    for i in 1:n_sites
+        plot!(Ω[:,i], label="final signal $i", lw=2)
+    end
+
+    savefig("final_signals_$(n_sites)_$(n_levels).pdf")
 # end
 # optimize()
